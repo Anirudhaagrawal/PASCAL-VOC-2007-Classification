0e553b2ab8f4e4e21b8bd7e420cca3840a501b67
Somansh <somanshbudhwar@gmail.com>
2023-02-22 13:28:42 -0800

    iou image-wise interpretation

--- util.py
+++ util.py
@@ -4,40 +4,6 @@ import pandas as pd
 import torch
 
 
-# def iou(pred, target, n_classes: int = 21) -> float:
-#     """
-#     Compute the Intersection-over-Union (IoU) of a single pattern, where
-#     for some object class k:
-#
-#     pixels(pattern, k) = set of pixels in the given pattern labeled class k
-#
-#     IoU of k = intersect(pixels(pred, k), pixels(target, k)) / union(pixels(pred, k), pixels(target, k))
-#
-#     After computing the IoU for each class, we simply average over each class to
-#     obtain a general IoU for the entire pattern.
-#
-#     :param pred: array containing the pixel predictions of the model
-#     :param target: array containing the class labels of the input
-#     :param n_classes: The expected number of classes in the pattern
-#     :return: The IoU of the pattern as a float
-#     """
-#     # pre-process pred and target, some entries will have value 255,
-#     # denoting object boundaries. set to 0, denoting background class
-#     pred[pred == 255] = 0
-#     target[target == 255] = 0
-#     # init empty array to hold IoU for each class
-#     iou = np.zeros(n_classes)
-#     # iterate through each class
-#     for k in range(n_classes):
-#         # calc the union of the pixels in pred and target labeled class k
-#         union = np.count_nonzero(np.logical_or(pred == k, target == k))
-#         union = union + 1e-10
-#         # calc the intersection of the pixels in pred and target labeled class k
-#         intersect = np.count_nonzero(np.logical_and(pred == k, target == k))
-#         # calc the IoU for class k
-#         iou[k] = intersect / union
-#     # return the mean IoU of the pattern
-#     return np.average(iou)
 def iou_per_image(pred, target):
     ious = []
     total = target.numel()
